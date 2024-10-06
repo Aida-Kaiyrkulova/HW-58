1d5d31e0e8afbcc51a3750dda1a2d0b1345fe165
Aida-Kaiyrkulova <aida-kaiyr@yandex.ru>
2024-10-06 22:35:25 +0600

    Add UI component Alert

---
+++ src/components/UI/Alert/Alert.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface Props {
+  type: 'primary' | 'success' | 'danger' | 'warning';
+  onDismiss?: () => void;
+  children: React.ReactNode;
+}
+
+const Alert: React.FC<Props> = ({type, onDismiss, children} )=> {
+  const alertClass = `alert alert-${type} mt-2 d-sm-flex justify-content-between`;
+
+  return (
+    <div className={alertClass} role="alert">
+      {children}
+<button type="button" className="close" onClick={onDismiss} aria-label="close"><span aria-hidden="true">x</span></button>
+    </div>
+  );
+};
+
+export default Alert;
